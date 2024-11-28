826fea919be52caa422681e1b9c998da1534d782
Daniel Chin <danielchin@Daniels-MacBook-Pro.local>
2024-11-28 20:57:09 +0900

    added empty components

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,15 +1,21 @@
-
-
+import {useState} from 'react';
 import './App.css'
 
 function App() {
-  
+  const [counter, setCounter] = useState(0)
 
-  return (
-    <>
-  
-    </>
-  )
+
+return (
+  <>
+    <button onClick={() => setCounter((counter) => counter + 1)}>
+      button : {counter}
+    </button>
+
+    <button onClick={() => setCounter((0))}>
+      clear
+    </button>
+  </>
+)
 }
 
 export default App
